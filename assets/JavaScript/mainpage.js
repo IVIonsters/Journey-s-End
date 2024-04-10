15fccc6408e4f740ca018a83c9554376b6e9866a
Zachary Polof <ZacharyPolof@gmail.com>
2024-04-10 15:48:11 -0500

    mainpage.css changes , mainpaige. js changes
    changed transform on slider.forward thumbnail to 150px from -150 to have image animation other direction.
    
    mainpage.js - added else to if else statement for back button trigger , modified animation runtime as well.

--- assets/JavaScript/mainpage.js
+++ assets/JavaScript/mainpage.js
@@ -8,11 +8,28 @@ forwardButton.onclick = function() {
     moveSlider('forward');
 }
 
+backButton.onclick = function() {
+    moveSlider('back');
+}
+
+let animationTime = 3000;
+let animationTimeOut;
+
 function moveSlider(type) {
     let sliderItems = document.querySelectorAll('.slider .list .item');
-    let thumbnailItems = document.querySelectorAll('.slider .thumbnail .item');
+    let thumbnailItems = document.querySelectorAll('.slider .thumbnail .card');
 
     if (type === 'forward') {
         listDOM.appendChild(sliderItems[0]);
+        thumbnails.appendChild(thumbnailItems[0]);
+        sliderDOM.classList.add('forward');
+    } else {
+        let positionLastThumbnail = thumbnailItems.length - 1;
+        listDOM.prepend(sliderItems[positionLastThumbnail]);
     }
+
+    clearTimeout(animationTimeOut);
+    animationTimeOut = setTimeout(() => {
+        sliderDOM.classList.remove('forward');
+    }, animationTime);
 }
