c6841b8b6ac5375ec9a441ca83a0e09aab76d17a
Joseph Ruff <joegruff16@gmail.com>
2024-04-10 21:40:40 -0400

    Update scriptTM.js

--- assets/JavaScript/scriptTM.js
+++ assets/JavaScript/scriptTM.js
@@ -16,17 +16,17 @@ const formSubmitHandler = function (event) {
 
 // searchQuery randomizer
 const randomizeSQTopic = function () {
-// searchQuery topic array
-const searchQueryTopics = [
-  'date',
-  'restaurants',
-  'fun',
-  'adventure',
-  'romantic',
-  'friends',
-  'activities',
-  'games'
-]
+  // searchQuery topic array
+  const searchQueryTopics = [
+    'date',
+    'restaurants',
+    'fun',
+    'adventure',
+    'romantic',
+    'friends',
+    'activities',
+    'games'
+  ]
 
   const randomSQTopic = searchQueryTopics[Math.floor(Math.random() * searchQueryTopics.length)];
 
@@ -41,31 +41,46 @@ const citySetting = function () { };
 //   retrieve data from ticketmaster.com API
 const getSuggestions = function () {
 
-    const cityEl = document.getElementById('city');
+  const cityEl = document.getElementById('city');
 
-    let city = cityEl.value;
-    const requestOptions = {
+  let city = cityEl.value;
+  const requestOptions = {
     method: "GET",
     redirect: "follow"
   };
-  
+
 
   fetch(`https://app.ticketmaster.com/discovery/v2/events.json?&city=${city}&apikey=PX9R1m9GGLoYMZnyzl7zDQT2EZJyjBqX`, requestOptions)
-  .then(response => response.json())
-  // store response in local storage
-  .then(response => localStorage.setItem('erosEvents', JSON.stringify(response)))
-  // error handling (basic)
-  .catch(err => console.error(err));
+    .then(response => response.json())
+    // store response in local storage
+    .then(response => localStorage.setItem('erosEvents', JSON.stringify(response)))
+    // error handling (basic)
+    .catch(err => console.error(err));
+
+  fetch(`https://app.ticketmaster.com/discovery/v2/attractions.json?&keyword=${city}&apikey=PX9R1m9GGLoYMZnyzl7zDQT2EZJyjBqX`, requestOptions)
+    .then(response => response.json())
+    // store response in local storage
+    .then(response => localStorage.setItem('erosAttractions', JSON.stringify(response)))
+    // error handling (basic)
+    .catch(err => console.error(err));
+
+  fetch(`https://app.ticketmaster.com/discovery/v2/suggest.json?&keyword=${city}&apikey=PX9R1m9GGLoYMZnyzl7zDQT2EZJyjBqX`, requestOptions)
+    .then(response => response.json())
+    // store response in local storage
+    .then(response => localStorage.setItem('erosSuggest', JSON.stringify(response)))
+    // error handling (basic)
+    .catch(err => console.error(err));
+
 };
 
 
 //   display resutls
 const displayResults = function () {
 
-let erosEvents = JSON.parse(localStorage.getItem("erosEvents")) || [];
+  let erosEvents = JSON.parse(localStorage.getItem("erosEvents")) || [];
 
-// Strategy >> loop through list of events/attractions/suggestions
-console.log(erosEvents.page._embedded) 
+  // Strategy >> loop through list of events/attractions/suggestions
+  console.log(erosEvents.page._embedded)
 
 
 }
