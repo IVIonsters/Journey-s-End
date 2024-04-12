0ba011d4c5b193a979f2face6114f6e1d0184eda
Joseph Ruff <joegruff16@gmail.com>
2024-04-11 20:47:57 -0400

    Deleted all code

--- assets/JavaScript/weather.js
+++ assets/JavaScript/weather.js
@@ -1,4 +1,1 @@
 // Weather API variables
-const cardWeather = document.querySelector(".cardWeather");
-const cityInput = document.querySelector(".cityInput");
-const apiKey = "d05fd26f2817ec3462a382b376a3f982";
