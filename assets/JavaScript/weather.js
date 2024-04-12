d777136a019ef9db7d2c61eabba41c7b9b55fce9
Joseph Ruff <joegruff16@gmail.com>
2024-04-11 20:08:47 -0400

    .

---
+++ assets/JavaScript/weather.js
@@ -0,0 +1,4 @@
+// Weather API variables
+const cardWeather = document.querySelector(".cardWeather");
+const cityInput = document.querySelector(".cityInput");
+const apiKey = "d05fd26f2817ec3462a382b376a3f982";
