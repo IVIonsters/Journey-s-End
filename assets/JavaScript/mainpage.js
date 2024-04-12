cfffdd207dc0143a0d4f6b1a827e0d62ec426aa1
Stephanie Carr <sacarr91@gmail.com>
2024-04-12 17:18:11 -0400

    Cleaned up my mess
    Animation autorun time halved

--- assets/JavaScript/mainpage.js
+++ assets/JavaScript/mainpage.js
@@ -10,7 +10,7 @@ let time1 = document.querySelector('.slider .time');
 
 thumbnail1.appendChild(thumbnail2[0]);
 let animationRunning = 3000;
-let autoRun = 100000;
+let autoRun = 50000;
 
 nextArrow.onclick = function(){
     showSlider('next');    
