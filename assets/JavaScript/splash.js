f2baa53b6b906866b856b7571eb6faf79a6a8d8e
Zachary Polof <ZacharyPolof@gmail.com>
2024-04-17 11:41:23 -0500

    comments
    updated comments in pages for presentation notes as well as for better reference for future changes

--- assets/JavaScript/splash.js
+++ assets/JavaScript/splash.js
@@ -1,14 +1,17 @@
+// defined variables
 let splash = document.querySelector('.splashpage');
 let sloganSpan = document.querySelectorAll('.slogan');
 
+// event listenter to trigger Timeout animations on content load
 window.addEventListener('DOMContentLoaded', () => {
+    // timeout delay before adding active class to sloganSpan
     setTimeout(() => {
         sloganSpan.forEach((span, idx) => {
             setTimeout(() => {
                 span.classList.add('active');
             }, (idx + 1) * 800)
         });
-
+    // timeout delay before removing active and setting fade class from css
         setTimeout(() => {
             sloganSpan.forEach((span, idx) => {
                 setTimeout(() => {
@@ -17,9 +20,16 @@ window.addEventListener('DOMContentLoaded', () => {
                 }, 6000)
             })
         }, 200);
-
+        // timeout delay before remove splash page entirely
         setTimeout(() => {
             splash.style.top = '-100vh';
         }, 7000)
     })
 })
+
+// Commented out as notes for presentation.
+// I wanted to create a splash page that would display a series of slogans in a sequence,
+// then fade out and remove itself from the DOM.
+// I set a timeout function to delay adding different classes and removing them to create
+// the appearance of a sequence of animations.
+// I then used a final timeout function to remove the splash page from the DOM entirely.
